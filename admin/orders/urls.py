96904d4848afc77e78b4832954432ff520537848
frankmaruf <frankmaruf@hotmail.com>
2021-05-23 11:16:26 +0600

    djagno-admin

---
+++ admin/orders/urls.py
@@ -0,0 +1,10 @@
+from .views import OrderGenericAPIView, ExportAPIView, ChartAPIView
+from django.urls import path
+
+
+urlpatterns = [
+    path('orders/', OrderGenericAPIView.as_view()),
+    path('orders/<str:pk>/', OrderGenericAPIView.as_view()),
+    path('export/', ExportAPIView.as_view()),
+    path('chart/', ChartAPIView.as_view()),
+]
---
+++ admin/products/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.urls import path
+from django.conf.urls.static import static
+from .views import ProductGenericAPIView, FileUploadView
+
+urlpatterns = [
+    path("products/", ProductGenericAPIView.as_view(), name="products"),
+    path("products/<str:pk>", ProductGenericAPIView.as_view(), name="product"),
+    path('upload/', FileUploadView.as_view())
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
